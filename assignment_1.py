dd3107347208bc496e4ec396e5ea9311163f076d
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-07-28 18:11:43 +0530

    updated file for airflow assignment_3

--- assignment_1.py
+++ assignment_1.py
@@ -1,22 +1,43 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from airflow import DAG
 
-
-default_args={
-    'owner':'DAG_2',
-   'retry':2,
-    'retry_delay':timedelta(minute=4)
+default_args = {
+    'owner': 'DAG',
+    'retry': 2,
+    'retry_delay': timedelta(seconds=30)
 }
 
+
+def addition():
+    a = input("Enter the a:")
+    b = int(input("Enter the b:"))
+    c = a + b
+    print("Sum of addition:", c)
+
+
+def even_Number():
+    a = int(input("Enter the number:"))
+    if a % 2 == 0:
+        print(a, "it is even")
+    else:
+        print("it is not even")
+
+
 with DAG(
-    dag_id='first_dag_v1'
-    default_args=default_args,
-    description='This is our first dag'
-    start_date=datetime(2023,5,20,3),
-    schedule_interval='@daily'
-)as dag:
-    task1=pythonOperator(
+        dag_id='first_dag_v1'
+default_args=default_args,
+description='This is our first dag'
+start_date=datetime(2023, 5, 20, 3),
+schedule_interval='@daily'
+) as dag:
+    task1 = pythonOperator(
         task_id='first_task_python',
-        pythonCallable=
+        pythonCallable=addition,
         dag='dag'
     )
+    task2 = pythonOperator(
+        task_id='second_task_python',
+        pythonCallable=even_Number,
+        dag='dag'
+    )
+    task2 << task1
